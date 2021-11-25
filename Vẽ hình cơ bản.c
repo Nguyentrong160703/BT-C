876c307a36ae76fb8610a587998a93ae00d99861
Nguyễn Anh Trọng <92981684+Nguyentrong160703@users.noreply.github.com>
2021-11-25 16:51:38 +0700

    VẼ HÌNH

---
+++ Vẽ hình cơ bản.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+void veHinhVuong (int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+void veTamGiac1(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<i+1; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+void veTamGiac2(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-i; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+void veTamGiac3(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<i; j++){
+			printf(" ");
+			}
+		for(int j=0; j<n-i; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+void veTamGiac4(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-i-1; j++){
+			printf(" ");
+			}
+		for(int j=0; j<i+1; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+void veTamGiac5(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-i-1; j++){
+			printf(" ");
+			}
+		for(int j=0; j<2*i+1; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+void veTamGiac6(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<i; j++){
+			printf(" ");
+			}
+		for(int j=0; j<2*(n-i)-1;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int n;
+	printf("Nhap n: ");
+	scanf("%d",&n);
+	printf("---Hinh vuong---\n");
+	veHinhVuong(n);
+	printf("---Tam giac can 1---\n");
+	veTamGiac1(n);
+	printf("---Tam giac can 2---\n");
+	veTamGiac2(n);
+	printf("---Tam giac can 3---\n");
+	veTamGiac3(n);
+	printf("---Tam giac can 4---\n");
+	veTamGiac4(n);
+	printf("---Tam giac can 5---\n");
+	veTamGiac5(n);
+	printf("---Tam giac can 6---\n");
+	veTamGiac6(n);
+	}
+
+	
